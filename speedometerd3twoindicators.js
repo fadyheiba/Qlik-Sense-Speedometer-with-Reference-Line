1c1b80250ea7436a7e756e0d5df62bc16113288b
Fady Heiba <fadyheiba@gmail.com>
2016-03-18 11:19:34 -0400

    v1.1

--- speedometerd3twoindicators.js
+++ speedometerd3twoindicators.js
@@ -32,7 +32,7 @@ define(["jquery",
                 },
                 measures : {
                     uses : "measures",
-                    min : 2,
+                    min : 1,
                     max : 2
                 },
                 sorting : {
@@ -50,22 +50,31 @@ define(["jquery",
             console.log($element);
             console.log(layout);
 
+
             var qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
             var measureLabels = layout.qHyperCube.qMeasureInfo.map(function(d) {
                 return d.qFallbackTitle;
             });
 
+            var measure2 = 0;
             var data = qMatrix.map(function(d) {
-                return {
-                    "Metric1":d[0].qNum,
-                    "Metric2":d[1].qNum
+                if(d[1] == null){
+                    return {
+                        "Metric1":d[0].qNum
+                    }
+                }
+                else{
+                    measure2 = 1;
+                    return {
+                        "Metric1":d[0].qNum,
+                        "Metric2":d[1].qNum
+                    }
                 }
             });
 
             var width = $element.width();
             var height = $element.height();
             var id = "container_" + layout.qInfo.qId;
-
             if (document.getElementById(id)) {
                 $("#" + id).empty();
             }
@@ -73,12 +82,66 @@ define(["jquery",
                 $element.append($('<div />;').attr("id", id).width(width).height(height));
             }
 
-            viz(data,measureLabels,width,height,id);
+            
+            if(measure2 == 0){
+                vizwithoutref(data,measureLabels,width,height,id);
+            }
+            else if (measure2 == 1){
+                vizwithref(data,measureLabels,width,height,id);
+            }
         }
     };
 });
 
-var viz = function(data,labels,width,height,id) {
+var vizwithoutref = function(data,labels,width,height,id) {
+    var svg = d3.select("#"+id)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+
+    var gauge = iopctrl.arcslider()
+    .radius((width+height)/5.5)
+    .events(false)
+    .indicator(iopctrl.defaultGaugeIndicator);
+    gauge.axis().orient("in")
+        .normalize(true)
+        .ticks(12)
+        .tickSubdivide(4)
+        .tickSize(10, 8, 10)
+        .tickPadding(5)
+        .scale(d3.scale.linear()
+               .domain([0, 100])
+               .range([-3*Math.PI/4, 3*Math.PI/4]));
+
+    var segDisplay = iopctrl.segdisplay()
+    .width((width+height)/10)
+    .digitCount(3)
+    .negative(false)
+    .decimals(1);
+
+    var transform = "translate(";
+    var widthtransform = ((width+height)*1.15/3)/1.87;
+    var heighttransform = ((width+height)*3.18/5)/2;
+    var meterplacement = transform.concat(widthtransform, ",",heighttransform,")");
+
+    var kpi = data.map(function(d){return d.Metric1;});
+
+    svg.append("g")
+        .attr("class", "gaugered")
+        .call(gauge);
+
+    svg.append("g")
+        .attr("class", "segdisplayred")
+        .attr("transform", meterplacement)
+        .call(segDisplay);
+
+    segDisplay.value(kpi);
+    gauge.value(kpi);
+};
+
+
+var vizwithref = function(data,labels,width,height,id) {
     var svg = d3.select("#"+id)
     .append("svg")
     .attr("width", width)
